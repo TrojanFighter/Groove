8c8b887b8b1b8cbd7675dbb213312644ff7ee5a3
Sparky <timdao@hotmail.com>
2019-01-26 15:25:39 -0500

    Connect attempt

--- Assets/Main/Scripts/ParasitePlayerController.cs
+++ Assets/Main/Scripts/ParasitePlayerController.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 
 namespace TwitchPlaysParasite
 {
-	public class ParasitePlayerController : NetworkBehaviour
+	public class ParasitePlayerController : RewiredBase
 	{
+		[SyncVar] 
+		public Vector2 m_targetVector;
 
 		public override void OnStartServer()
 		{
 			base.OnStartServer();
+			//Initialize();
 		}
 
+		public override void OnStartClient()
+		{
+			Initialize();
+		}
+
+		protected override void Start()
+		{
+			//Initialize();
+		}
+
+
+		protected override void GetInput()
+		{
+			base.GetInput();
+			m_targetVector.x = player.GetAxis("Horizontal");
+			m_targetVector.y = player.GetAxis("Vertical");
+		}
 	}
 }
